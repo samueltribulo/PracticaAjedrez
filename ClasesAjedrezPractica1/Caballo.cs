1c1ca419670984dbe0ac92214f681f7966d28a1d
Martina <martinaceschan@gmail.com>
2023-01-11 17:03:49 -0300

    nueva clase hija

---
+++ ClasesAjedrezPractica1/Caballo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAjedrezPractica1
+{
+    public class Caballo:PiezaAjedrez
+    {
+        public override string Mover()
+        {
+            return "Movimiento en ele";
+        }
+    }
+}
---
+++ ClasesAjedrezPractica1/Torre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAjedrezPractica1
+{
+    public class Torre:PiezaAjedrez
+    {
+        public override string Mover()
+        {
+            return "Movimiento en línea recta o en horizontal";
+        }
+    }
+}
