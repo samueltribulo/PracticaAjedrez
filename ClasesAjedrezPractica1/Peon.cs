641969f8c31357492fc5727f68803b2959d1dc14
Martina <martinaceschan@gmail.com>
2023-01-11 16:59:50 -0300

    clase hija peón

---
+++ ClasesAjedrezPractica1/Peon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAjedrezPractica1
+{
+    public class Peon:PiezaAjedrez
+    {
+        public override string Mover()
+        {
+            return "Mover en línea recta";
+        }
+    }
+}
