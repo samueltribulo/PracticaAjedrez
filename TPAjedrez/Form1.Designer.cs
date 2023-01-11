314f6c184bddcd0d5853887a6b2fe5e2c529cc5e
Martina <martinaceschan@gmail.com>
2023-01-11 17:20:24 -0300

    terminado

--- TPAjedrez/Form1.Designer.cs
+++ TPAjedrez/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMoverTorre = new System.Windows.Forms.Button();
+            this.btnMoverCaballo = new System.Windows.Forms.Button();
+            this.btnMoverPeon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMoverTorre
+            // 
+            this.btnMoverTorre.Location = new System.Drawing.Point(515, 95);
+            this.btnMoverTorre.Name = "btnMoverTorre";
+            this.btnMoverTorre.Size = new System.Drawing.Size(156, 29);
+            this.btnMoverTorre.TabIndex = 0;
+            this.btnMoverTorre.Text = "Mover torre";
+            this.btnMoverTorre.UseVisualStyleBackColor = true;
+            this.btnMoverTorre.Click += new System.EventHandler(this.btnMoverTorre_Click);
+            // 
+            // btnMoverCaballo
+            // 
+            this.btnMoverCaballo.Location = new System.Drawing.Point(515, 151);
+            this.btnMoverCaballo.Name = "btnMoverCaballo";
+            this.btnMoverCaballo.Size = new System.Drawing.Size(156, 29);
+            this.btnMoverCaballo.TabIndex = 1;
+            this.btnMoverCaballo.Text = "Mover caballo";
+            this.btnMoverCaballo.UseVisualStyleBackColor = true;
+            this.btnMoverCaballo.Click += new System.EventHandler(this.btnMoverCaballo_Click);
+            // 
+            // btnMoverPeon
+            // 
+            this.btnMoverPeon.Location = new System.Drawing.Point(515, 202);
+            this.btnMoverPeon.Name = "btnMoverPeon";
+            this.btnMoverPeon.Size = new System.Drawing.Size(156, 29);
+            this.btnMoverPeon.TabIndex = 2;
+            this.btnMoverPeon.Text = "Mover peón";
+            this.btnMoverPeon.UseVisualStyleBackColor = true;
+            this.btnMoverPeon.Click += new System.EventHandler(this.btnMoverPeon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMoverPeon);
+            this.Controls.Add(this.btnMoverCaballo);
+            this.Controls.Add(this.btnMoverTorre);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btnMoverTorre;
+        private Button btnMoverCaballo;
+        private Button btnMoverPeon;
     }
 }
--- TPAjedrez/Form1.cs
+++ TPAjedrez/Form1.cs
@@ -1,3 +1,4 @@
+using ClasesAjedrezPractica1;
 namespace TPAjedrez
 {
     public partial class Form1 : Form
@@ -5,6 +6,28 @@ namespace TPAjedrez
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnMoverTorre_Click(object sender, EventArgs e)
+        {
+            Torre torre= new Torre();
+            MessageBox.Show(torre.Mover());
+
+        }
+
+        private void btnMoverCaballo_Click(object sender, EventArgs e)
+        {
+
+            Caballo caballo = new Caballo();
+            MessageBox.Show(caballo.Mover());
+        }
+
+        private void btnMoverPeon_Click(object sender, EventArgs e)
+        {
+
+
+            Peon peon = new Peon();
+            MessageBox.Show(peon.Mover());
         }
     }
 }
